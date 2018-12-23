575ec4035179b8444fc697f4f9221ee984e1ff72
CancerGary <12206312+CancerGary@users.noreply.github.com>
2018-12-23 16:38:22 +0800

    [frontend] extract imperium treeview code

--- src/components/ImperiumShow.vue
+++ src/components/ImperiumShow.vue
@@ -1,80 +1,27 @@
 <template>
-  <v-treeview :items="items"></v-treeview>
+  <ImperiumTreeview :imperium-data="imperiumData"></ImperiumTreeview>
 </template>
 
 <script>
+  import ImperiumTreeview from "./ImperiumTreeview";
+
   export default {
-    data: () => ({
-      items: []
-    }),
+    name: "ImperiumShow",
+    components: {ImperiumTreeview},
+    data() {
+      return {
+        imperiumData: {},
+      }
+    },
     created() {
       this.$http.get('/api/imperium/' + this.$route.params.imperium_id + '/unpack/').then(response => {
         console.log(response.data);
-
-        var id = 0;
-
-        function handle(root,alterKey) {
-          var result = [];
-
-          // fallback
-          if (root.constructor === Object || root.constructor === Array) {
-            for (var key in root) {
-              //debugger;
-              // special optimize
-              // C : [{keys:[],title:"",]
-              if (key==='C') {
-                var result_=[];
-                for (var i in root[key]) {
-                  var cObject=root[key][i];
-                  var keyIndex=cObject.keys.indexOf('Key');
-                  id+=1;
-                  var baseId=id;
-                  var result__=[];
-                  cObject.rows.forEach(function (item,index) {
-                    id+=1;
-                    result__.push({id:id,name:(keyIndex > -1 ?item[keyIndex]:index),children:handle(item,cObject.keys)})
-                  })
-                  result_.push({id:baseId,name:cObject.title,children:result__})
-                }
-                result.push({id:id,name:key+' [C]',children:result_});
-                continue;
-              }
-              id += 1;
-              if (root[key].constructor === Object || root[key].constructor === Array) {
-                result.push({id: id, name: key + ' [' + root[key].constructor.name + ']', children: handle(root[key])});
-              } else {
-                if (alterKey) result.push({id: id, name: key + ' [' + alterKey[key] + '] : ' + root[key]});
-                else result.push({id: id, name: key + ' : ' + root[key]});
-              }
-            }
-          } else {
-            id += 1;
-            return {id: id, name: root.toString()}
-          }
-          return result;
-        }
-
-        var d = handle(response.data);
-        console.log(d);
-        this.items = d;
+        this.imperiumData = response.data;
       })
     }
-
   }
 </script>
 
 <style scoped>
-  >>>.v-treeview-node{
-    margin-left:10px!important;
-  }
-  >>>.v-treeview-node__root{
-    padding-top: 4px!important;
-    height: auto!important;
-  }
-  >>>.v-treeview-node__content{
-    width: 100px!important;
-  }
-  >>>.v-treeview-node__label{
-    width: 100px!important;
-  }
+
 </style>
