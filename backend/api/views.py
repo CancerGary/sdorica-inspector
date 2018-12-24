6031cafd0df47257bbd4d4fa8ecbbe4eb6685de7
CancerGary <12206312+CancerGary@users.noreply.github.com>
2018-12-24 10:07:19 +0800

    [backend] disable CSRF && add permissions to viewsets

--- backend/api/views.py
+++ backend/api/views.py
@@ -8,7 +8,7 @@ from django.views.generic import TemplateView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.cache import never_cache
-from rest_framework import viewsets,mixins
+from rest_framework import viewsets, mixins, permissions
 from rest_framework.decorators import action
 from rest_framework.response import Response
 
@@ -18,6 +18,14 @@ import hashlib
 
 # Serve Vue Application
 index_view = login_required(never_cache(TemplateView.as_view(template_name='index.html')))
+
+from rest_framework.authentication import SessionAuthentication, BasicAuthentication
+
+# disable csrf check
+class CsrfExemptSessionAuthentication(SessionAuthentication):
+
+    def enforce_csrf(self, request):
+        return
 
 # login page
 def login_view(request):
@@ -34,6 +42,9 @@ class GameVersionViewSet(viewsets.ModelViewSet):
     """
     API endpoint that allows game versions to be viewed or edited.
     """
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+    permission_classes = (permissions.IsAuthenticated,)
+
     queryset = GameVersion.objects.all()
     serializer_class = GameVersionSerializer
 
@@ -46,6 +57,9 @@ class ImperiumViewSet(viewsets.ModelViewSet):
     serializer_class = ImperiumSerializer
     filter_backends = (DjangoFilterBackend,)
     filter_fields = ('game_version', 'type_id')
+
+    authentication_classes = (CsrfExemptSessionAuthentication, BasicAuthentication)
+    permission_classes = (permissions.IsAuthenticated,)
 
     @action(detail=True)
     def unpack(self,request, *args, **kwargs):
