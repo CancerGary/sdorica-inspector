52309c74b91a0b9570bda823e6d0a70d89738f77
CancerGary <12206312+CancerGary@users.noreply.github.com>
2019-02-03 16:31:50 +0800

    [backend] run as background process

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
 eval $(ps -ef | grep "[0-9] /home/sdorica-inspector" | awk '{print "kill "$2}')
-nohup pipenv run gunicorn backend.wsgi --log-file gunicorn.log &
-nohup pipenv run celery -A backend.api.celery worker --loglevel=info --logfile=celery.log --concurrency=4 &
+# run as background process
+pipenv run gunicorn backend.wsgi --log-file gunicorn.log --daemon
+pipenv run celery -A backend.api.celery worker --loglevel=info --logfile=celery.log --concurrency=4 --detach
