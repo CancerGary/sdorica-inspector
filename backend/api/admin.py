1b825290bcc2090875e99821a2fc7e93b662e0d5
CancerGary <12206312+CancerGary@users.noreply.github.com>
2019-10-08 21:54:33 +0800

    [B] Improve Django Admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 
 # Register your models here.
-from backend.api.models import GameVersion,Imperium
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
-admin.site.register(GameVersion)
-admin.site.register(Imperium)
+from backend.api.models import DiscordInvite
+
+
+class DiscordInviteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'discord_id']
+    search_fields = ['discord_id']
+
+
+class NewUserAdmin(UserAdmin):
+    list_display = ['username', 'discord_id', 'last_login']
+    list_select_related = ['profile']
+    search_fields = ['username', 'profile__discord_id']
+
+    def discord_id(self, obj):
+        return obj.profile.discord_id
+
+
+admin.site.register(DiscordInvite, DiscordInviteAdmin)
+admin.site.unregister(User)
+admin.site.register(User, NewUserAdmin)
